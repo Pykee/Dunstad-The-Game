5f7045a5ed2f8c64b1314604ffece179f18bbf00
Jacob <pykee.03@live.com>
2014-07-28 20:29:39 +1000

    Adding Main Files

---
+++ Dunstad The Game/Assets/Scripts/Player.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+    float x, y;
+    void Update()
+    {
+		if (networkView.isMine) {
+						if (Input.GetMouseButtonDown (1))
+								Screen.lockCursor = !Screen.lockCursor;
+
+						if (Screen.lockCursor) {
+								x += Input.GetAxis ("Mouse X") * 5;
+								y -= Input.GetAxis ("Mouse Y") * 4;
+
+								var rotation = Quaternion.Euler (y, x, 0);
+								transform.rotation = rotation;
+						}
+				}
+    }
+}
---
+++ Dunstad The Game/Assets/Scripts/Shooting.js
@@ -0,0 +1,21 @@
+﻿#pragma strict
+
+var projectile : Rigidbody;
+var speed = 20;
+var clone : Rigidbody;
+var hands : Transform;
+
+function Update () {
+		if (networkView.isMine) {
+									
+								if (Input.GetMouseButtonDown(0)){
+								
+									clone = Network.Instantiate(projectile, hands.position, hands.rotation, 0);
+									clone.velocity = transform.TransformDirection(Vector3(0, 0, speed));
+									
+									Network.Destroy(gameObject(2).viewID);
+						
+								}
+		
+					}
+}
